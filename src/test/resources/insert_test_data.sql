501ce89c6e9d79aa53dad73ebb5e62692dfc73a1
Iván Pérez <ivan.pf.89@gmail.com>
2023-02-04 21:12:10 +0100

    Swagger integration (+ native support) (#3)
    * swagger ui integration
    
    * swagger ui integration and Grafana dashboard improvement

--- src/test/resources/insert_test_data.sql
+++ src/test/resources/insert_test_data.sql
@@ -1,3 +1,5 @@
+DELETE FROM CARS;
+
 INSERT INTO CARS (brand, model, fuel_type, num_of_doors, price) VALUES ('audi', 'a5', 'GASOLINE', 5, 40000);
 INSERT INTO CARS (brand, model, fuel_type, num_of_doors, price) VALUES ('bmw', 'm4', 'GASOLINE', 5, 80000);
 INSERT INTO CARS (brand, model, fuel_type, num_of_doors, price) VALUES ('seat', 'leon', 'DIESEL', 3, 28000);
