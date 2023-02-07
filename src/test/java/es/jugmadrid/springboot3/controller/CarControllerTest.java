57c2bbb03d2ae0952969fd8297e7d9254095a15e
ivan_perez <ivan.pf.89@gmail.com>
2023-02-07 22:22:41 +0100

    added test config

--- src/test/java/es/jugmadrid/springboot3/controller/CarControllerTest.java
+++ src/test/java/es/jugmadrid/springboot3/controller/CarControllerTest.java
@@ -1,5 +1,8 @@
 package es.jugmadrid.springboot3.controller;
 
+import es.jugmadrid.springboot3.dao.repository.CarRepository;
+import es.jugmadrid.springboot3.model.FuelType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,6 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.math.BigDecimal;
+
+import static es.jugmadrid.springboot3.utils.TestFactoryUtils.buildCar;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +23,13 @@ class CarControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private CarRepository carRepository;
+
+    @BeforeEach
+    void setup(){
+        insertTestData();
+    }
 
     @Test
     void givenData_whenGetCarsFiltered_thenFilteredResponseExpected() throws Exception {
@@ -26,7 +39,12 @@ class CarControllerTest {
                         .queryParam("fuelType", "DIESEL"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content..fuelType", everyItem(is("DIESEL"))))
-                .andExpect(jsonPath("$.content..price", everyItem(greaterThan(10000))))
-                .andExpect(jsonPath("$.content..price", everyItem(lessThan(15000))));
+                .andExpect(jsonPath("$.content..price", everyItem(greaterThan(10000.00))))
+                .andExpect(jsonPath("$.content..price", everyItem(lessThan(15000.00))));
+    }
+
+    private void insertTestData(){
+        carRepository.save(buildCar("Seat", "Ibiza", BigDecimal.valueOf(14000.00), FuelType.DIESEL, 3));
+        carRepository.save(buildCar("Seat", "León", BigDecimal.valueOf(24000.00), FuelType.DIESEL, 5));
     }
 }
